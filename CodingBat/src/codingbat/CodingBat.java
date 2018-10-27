ff37334d8b35bc5e8068706b6f7e9da70e7bfd04
Anna Nikolaeva <amnik21@yandex.ru>
2018-10-27 12:48:11 +0300

    Упражнения со строками с сайта CodingBat

--- CodingBat/src/codingbat/CodingBat.java
+++ CodingBat/src/codingbat/CodingBat.java
@@ -2,63 +2,19 @@ package codingbat;
 
 public class CodingBat {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {        
         CodingBat exercise = new CodingBat();
-        int intMax = exercise.intMax(1, 2, 3);
-        System.out.println(intMax);
-        
-        CodingBat exercise1 = new CodingBat();
-        int close10 = exercise1.close10(7, 13);
-        System.out.println(close10);
-
-        CodingBat exercise2 = new CodingBat();
-        boolean stringE = exercise2.stringE("Hello");
-        System.out.println(stringE);
-
-        CodingBat exercise3 = new CodingBat();
-        String everyNth = exercise3.everyNth("Привет, мир", 2);
-        System.out.println(everyNth);
+        boolean doubleX = exercise.doubleX("axxaaxx");
+        System.out.println(doubleX);
     }
     
-    public int intMax(int a, int b, int c) {
-        if (a > b && a > c) {
-            return a;
-        } else if(b > a && b > c) {
-            return b;
-        } 
-            return c;
-    }
-    
-    public int close10(int a, int b) {
-        int x = 10 - a;
-        int y = 10 - b;
-        if (Math.abs(x) < Math.abs(y)) {
-            return a;
-        } if (Math.abs(x) > Math.abs(y)) {
-            return b;
-        } return 0;
-    }
-
-    public boolean stringE(String str) {
-        int j = 0;
+    boolean doubleX(String str) {
+        int ind = str.indexOf("x");
         for (int i = 0; i < str.length(); i ++) {
-            if (str.charAt(i) == 'e') {
-                j += 1;
+            if (ind != -1 && str.substring(ind, ind + 2).equals("xx")) {
+                return true;
             }
-        }
-        if (j <= 3 && j >= 1) {
-            return true;
         }
         return false;
     }
-    
-    public String everyNth(String str, int n) {
-        String newStr = "";
-        for (int i = 0; i < str.length(); i = i + 2) {
-            newStr += str.charAt(i);
-        }
-        return newStr;
-    }
-
 }
